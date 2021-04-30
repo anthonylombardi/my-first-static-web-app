9b9c5358dfc64ac0a5e371736e29ec7d530494cb
anthonylombardi <anthony.lombardi@virtuaule.com>
2021-04-30 12:28:02 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  const value = 'World my-first-static';
+  const value = 'World my-first-static cloned from git';
   return <div>Hello {value}</div>;
 }
 
